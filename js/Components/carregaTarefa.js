8d5f27a446fe5144a8cdb634593ee3d4435bf004
Daniel Carlos <84096317+danielcarlos-s@users.noreply.github.com>
2021-08-17 18:42:32 -0300

    Funcionalidades
    todas as funcionalidades foram aplicadas
     - Criar tarefa e data
     - Concluir tarefa
     - carregar tarefa
     - deleta tarefa

--- js/Components/carregaTarefa.js
+++ js/Components/carregaTarefa.js
@@ -1,3 +1,4 @@
+import { ordenaDatas, removeDatasRepetidas } from "../../service/data.js";
 import { criaData } from "./criaData.js"
 
 export const carregaTarefa = () => {
@@ -6,9 +7,9 @@ export const carregaTarefa = () => {
     const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas')) || [];
 
     lista.innerHTML = " ";
-
-    tarefasCadastradas.forEach((tarefa) => {
-        const dia = moment(tarefa.dataFormatada, 'DD/MM/YYYY')
+    const dataUnicas = removeDatasRepetidas(tarefasCadastradas)
+    ordenaDatas(dataUnicas)
+    dataUnicas.forEach((dia) => {
         lista.appendChild(criaData(dia))
     })
 }
